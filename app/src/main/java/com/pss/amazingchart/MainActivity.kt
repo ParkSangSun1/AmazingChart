015ca667297fc0fd065dcf6e7996da5bc0899398
ParkSangSun1 <appleasz1028102@gmail.com>
2024-02-21 09:45:45 +0900

    feat: y축 비율에 따른 시간 표현

--- app/src/main/java/com/pss/amazingchart/MainActivity.kt
+++ app/src/main/java/com/pss/amazingchart/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.pss.amazingchart
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -34,7 +41,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AmazingChart()
+                    AmazingChart(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
@@ -44,7 +53,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AmazingChart(
     modifier: Modifier = Modifier,
-    timeInterval: Int = 10,
+    //0.5 = 30분
+    timeInterval: Double = 0.5,
     topBlank: Int = 0,
     bottomBlank: Int = 0,
     chartDataList: List<DayChartBar> = listOf(
@@ -64,7 +74,6 @@ fun AmazingChart(
         )
     )
 ) {
-
     val colors = listOf(Color.Black, Color.Red, Color.Blue, Color.Green)
     val textMeasurer = rememberTextMeasurer()
     val textToDraw = "30:30"
@@ -83,12 +92,14 @@ fun AmazingChart(
     //가장 높은 시간과 가장 낮은 시간 계산
     for (item in chartDataList) {
         val highTime = if (sdf.format(item.actualSleep.topTimeStamp)
-                .toInt() > sdf.format(item.estimatedSleep.topTimeStamp).toInt())
+                .toInt() > sdf.format(item.estimatedSleep.topTimeStamp).toInt()
+        )
             sdf.format(item.actualSleep.topTimeStamp).toInt()
         else sdf.format(item.estimatedSleep.topTimeStamp).toInt()
 
         val lowTime = if (sdf.format(item.actualSleep.bottomTimeStamp)
-                .toInt() > sdf.format(item.estimatedSleep.bottomTimeStamp).toInt())
+                .toInt() > sdf.format(item.estimatedSleep.bottomTimeStamp).toInt()
+        )
             sdf.format(item.estimatedSleep.bottomTimeStamp).toInt()
         else sdf.format(item.actualSleep.bottomTimeStamp).toInt()
 
@@ -98,31 +109,62 @@ fun AmazingChart(
     topList.sortByDescending { it }
     bottomList.sortByDescending { it }
 
-    Log.d("carly","top : ${topList[0]}, bottom : ${bottomList[0]}")
+    Log.d("carly", "top : ${topList[0].toHour()}, bottom : ${bottomList[0].toHour()}")
 
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        for (y in 1..5) {
 
-            drawText(
-                textMeasurer = textMeasurer,
-                text = textToDraw,
-                style = textStyle,
-                topLeft = Offset(
-                    x = 0.dp.toPx(),
-                    y = ((y * 100).dp.toPx()) - (textLayoutResult.size.height / 2)
+
+
+    Box(modifier = modifier) {
+        Canvas(modifier = modifier) {
+
+            //가장 높은 top 값과 가장 낮은 bottom 값의 사이 공간 값
+            val interval = topList[0].toHour() - bottomList[0].toHour()
+
+            //interval 값을 사용자가 정의한 간격 높이로 몇 번 그릴지
+            val lineNumber = (interval / timeInterval)
+
+            //line 간의 간격
+            val lineHeightInterval = this.size.height.dp.value / lineNumber
+
+            Log.d(
+                "carly",
+                "canvas : ${this.size.width.toDp()}, ${size.height.toDp()}, $lineHeightInterval"
+            )
+
+            for (y in 1..lineNumber.toInt()) {
+
+                Log.d("carly", "for : ${lineHeightInterval * y}")
+                drawText(
+                    textMeasurer = textMeasurer,
+                    text = textToDraw,
+                    style = textStyle,
+                    topLeft = Offset(
+                        x = 0.dp.toPx(),
+                        y = ((lineHeightInterval * y) - (textLayoutResult.size.height / 2)).toFloat()
+                    )
                 )
-            )
 
-            drawLine(
-                Color.Black,
-                start = Offset(50.dp.toPx(), (y * 100).dp.toPx()),
-                end = Offset(400.dp.toPx(), (y * 100).dp.toPx()),
-                strokeWidth = 5.dp.toPx(),
-                cap = StrokeCap.Square,
-                pathEffect = PathEffect.dashPathEffect(floatArrayOf(20f, 40f), 0f)
-            )
+                drawLine(
+                    Color.Black,
+                    start = Offset(50.dp.toPx(), (y * 100).dp.toPx()),
+                    end = Offset(400.dp.toPx(), (y * 100).dp.toPx()),
+                    strokeWidth = 5.dp.toPx(),
+                    cap = StrokeCap.Square,
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(20f, 40f), 0f)
+                )
+            }
         }
     }
+
+}
+
+val Float.dpInt: Int
+    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+fun Int.toHour(): Int {
+    val time = this.toString()
+    return if (time.length > 3) time.substring(0, 2).toInt()
+    else time.substring(0, 1).toInt()
 }
 
 @Preview(showBackground = true)
